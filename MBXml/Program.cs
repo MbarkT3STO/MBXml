fba5673fd34fd6ac4e70b1128004f3cf79f338ee
MBARK T3STO <mbark@t3sto.com>
2021-01-13 17:22:59 +0100

    Add project files.

---
+++ MBXml/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MBXml
+{
+    class Program
+    {
+
+        private static readonly List<Order> Orders = new List<Order>
+                                                     {
+                                                         new Order(1, "Ennasiri Ali", "PRD-1", 1500),
+                                                         new Order(2, "Badaoui Inas", "PRD-1", 2000),
+                                                         new Order(3, "Baddouh Ali", "PRD-3", 1000),
+                                                         new Order(4, "Mouslim Kawtar", "PRD-2", 3500),
+                                                         new Order(5, "Essalmi Karim", "PRD-1", 2000),
+                                                         new Order(6, "Nousayr Ahmed", "PRD-1", 2000),
+                                                         new Order(7, "Mersaoui Fatima", "PRD-3", 1000),
+                                                         new Order(8, "Fanar Adil", "PRD-1", 2200),
+                                                         new Order(9, "Eddawdi Nawal", "PRD-2", 3200),
+                                                         new Order(10, "Houmam Karim", "PRD-1", 2400),
+                                                         new Order(11, "Ennasiri Ali", "PRD-2", 2000),
+                                                         new Order(12, "Ennasiri Ali", "PRD-3", 3500),
+                                                         new Order(13, "Essalmi Karim", "PRD-2", 1500),
+                                                         new Order(14, "Eddawdi Nawal", "PRD-1", 2000)
+                                                     };
+
+        static void Main(string[] args)
+        {
+
+            XDocument doc = new XDocument(new XElement("Orders",
+                                                       new XElement("Client", "CLT-1"),
+                                                       new XElement("Total", "5000 MAD")));
+            doc.Save(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\xxx.xml");
+
+            Console.WriteLine("Saved");
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ MBXml/XMExporter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MBXml
+{
+
+    /// <summary>
+    /// To Export data as an XML file
+    /// </summary>
+    class XMExporter
+    {
+
+        private bool _Export<T>(List<T> dataAsList, string rootElementName,string path)
+        {
+            XDocument xdoc = new XDocument(new XElement(rootElementName));
+
+            //Get DataAsList parameter type number of properties
+            int PropertiesLength = typeof(T).GetProperties().Length;
+            
+            //Get DataAsList parameter type properties
+            var dataAsList_Properties = typeof(T).GetProperties();
+
+
+            foreach (var d in dataAsList)
+            {
+                
+            }
+
+
+        }
+
+    }
+}
